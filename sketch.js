f786e98dc2bad822606e7c8018180fdd08ed9cb1
LucasOHP <92893161+LucasOHP@users.noreply.github.com>
2021-10-20 18:57:47 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup(){
+  createCanvas(400,400);
+  }
+
+  
+function draw()
+{
+  background('lightgray');
+  textSize(35);
+  textAlign(CENTER,CENTER);
+  fill('blue');
+  text('Olá, mundo!',200,200);
+}
